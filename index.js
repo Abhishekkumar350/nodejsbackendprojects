2779811cf9b9b76397723fafee25e748e6a50e4a
abhishek saharan <abhisheksaharan@192.168.1.77>
2025-12-31 20:08:45 +0530

    learning_node.js_with_tutor_and_building

--- index.js
+++ index.js
@@ -12,27 +12,37 @@ fs.readFile(`index.js`,'utf-8',(err,data)=>{
     console.log(data);
 */
 //serever
-const tempOver = fs.readFileSync(`${__dirname}/templates/templates-overview.html`,'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/templates-card.html`,'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/templates-product.html`,'utf-8');
+const tempOver = fs.readFileSync(`${__dirname}/starter/templates/templateoverview.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/templateproduct.html`,'utf-8');
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`,'utf-8');
 const dataObj= JSON.parse(data);
 const server = http.createServer((req,res)=>{
     const pathName = req.url;
 //overview page
     if(pathName==='/' || pathName==='/overview'){
-        res.end(tempOverview);
+        res.end(tempOver);
 //product page
     }else if (pathName==='/product'){
         res.end('This is the PRODUCT');
+    }else if (pathName==='/api'){
+       
+        res.writeHead(200,{'content-type':'application/json'})
+        res.end(data);
+       }else{
+            res.writeHead(404,{
+            'constent-type': 'text/html',
+            'my-own-header': 'hello-world',
 
-    }else{
-        res.writeHead(404,{
-            'constent-type': 'text/html'
-        });
+        })
         res.end('<h1>PAGE NOT FOUND!</h1>')
     }
+
     
 });
-server.listen()
+
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('Listening to req on port 8000')
+});
