73c18e7e4d3e262be2d03963bdbbdcf698d8b128
abhishek saharan <abhisheksaharan@192.168.1.77>
2026-01-01 11:26:33 +0530

    these are the new changes

--- index.js
+++ index.js
@@ -2,16 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-/** 
-const textIn = fs.readFileSync('starter/txt/input.txt','utf-8');
-const textOut = `This is what we know about the avacado : ${textIn}.\n Created on ${Date.now()}`;
-fs.writeFileSync('starter/txt/output.txt', textOut);
-console.log('File written!');
 
-fs.readFile(`index.js`,'utf-8',(err,data)=>{
-    console.log(data);
-*/
-//serever
+
 const replaceTemplate = (temp, product)=>{
     let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
     output = output.replace(/{%IMAGE%}/g,product.image);
@@ -21,20 +13,31 @@ const replaceTemplate = (temp, product)=>{
     output = output.replace(/{%QUANTITY%}/g,product.quantity);
     output = output.replace(/{%DESCRIPTION%}/g,product.description);
     output = output.replace(/{%ID%}/g,product.id);
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-orgainc');
+    if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  } else {
+    output = output.replace(/{%NOT_ORGANIC%}/g, '');
+  }
+
     return output;
 
 }
+
+
+
 const tempOver = fs.readFileSync(`${__dirname}/starter/templates/templateoverview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`,'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/templateproduct.html`,'utf-8');
-
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`,'utf-8');
+
 const dataObj= JSON.parse(data);
+
+//creatingserever
 const server = http.createServer((req,res)=>{
-    const pathName = req.url;
+    const {query, pathname } = url.parse(req.url, true);
+
 //overview page
-    if(pathName==='/' || pathName==='/overview'){
+    if(pathname==='/' || pathname==='/overview'){
         res.writeHead(200,{'content-type':'text/html'});
 
         const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -42,10 +45,14 @@ const server = http.createServer((req,res)=>{
         console.log(cardHtml);
         res.end(output);
 //product page
-    }else if (pathName==='/product'){
-        res.end('This is the PRODUCT');
+    }else if (pathname==='/product'){
+        res.writeHead(200,{'content-type':'text/html'});
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct,product)
+
+        res.end(output);
 //api
-    }else if (pathName==='/api'){
+    }else if (pathname==='/api'){
        
         res.writeHead(200,{'content-type':'application/json'});
         res.end(data);
