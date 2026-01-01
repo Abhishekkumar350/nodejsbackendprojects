e8b08d42a2bc85084b5c266335c35e620d5f23bc
abhishek saharan <abhisheksaharan@192.168.1.77>
2026-01-01 10:41:32 +0530

    new modification

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ fs.readFile(`index.js`,'utf-8',(err,data)=>{
     console.log(data);
 */
 //serever
+const replaceTemplate = (temp, product)=>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output = output.replace(/{%IMAGE%}/g,product.image);
+    output = output.replace(/{%PRICE%}/g,product.price);
+    output = output.replace(/{%FROM%}/g,product.from);
+    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g,product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g,product.description);
+    output = output.replace(/{%ID%}/g,product.id);
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-orgainc');
+    return output;
+
+}
 const tempOver = fs.readFileSync(`${__dirname}/starter/templates/templateoverview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`,'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/templateproduct.html`,'utf-8');
@@ -22,14 +35,19 @@ const server = http.createServer((req,res)=>{
     const pathName = req.url;
 //overview page
     if(pathName==='/' || pathName==='/overview'){
-        res.end(tempOver);
+        res.writeHead(200,{'content-type':'text/html'});
+
+        const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOver.replace('{%PRODUCT_CARDS%}', cardHtml);
+        console.log(cardHtml);
+        res.end(output);
 //product page
     }else if (pathName==='/product'){
         res.end('This is the PRODUCT');
 //api
     }else if (pathName==='/api'){
        
-        res.writeHead(200,{'content-type':'application/json'})
+        res.writeHead(200,{'content-type':'application/json'});
         res.end(data);
 //NOT FOUND
 
